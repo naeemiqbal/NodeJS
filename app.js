a09e1bc6f6055d11918165bb47ada582eaeb077d
Naeem Iqbal <naeemiqbal@hotmail.com>
2022-03-12 16:44:59 -0500

    2nd

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const hostname = '127.0.0.1';
-const port = 7878;
+const port = 80;
 const static = "./static";
 const server = http.createServer((req, res) => {
     console.log(req.url);
